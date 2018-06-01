42fde8db6dd363a8bf0603b7c33b575e9846475a
Shushanik Sargsyan <shushan.sargsyan@gmail.com>
2018-06-01 17:48:13 +0400

    autoload function working correctly

--- public/index.php
+++ public/index.php
@@ -1,13 +1,22 @@
 <?php
 
-include "Router.php";
+//include "../core/Router.php";
+//include "../core/Request.php";
+
+function __autoload($classname) {
+    $filename = "../core/". $classname .".php";
+    include_once($filename);
+}
+
 $query = rtrim($_SERVER['QUERY_STRING'],'/');
 
 Router::add("^$", ['controller' => "Main", 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 
-$request = new Request();
+/** @var object $header */
+
+$header= new Header();
 $key = 'host';
 
-echo '<pre>',print_r($request->all()),'</pre>';
+echo '<pre>',print_r($header->all()),'</pre>';
 ?>
