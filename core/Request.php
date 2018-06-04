6fda73c2bbf75a228a00ca531c4a55b9d74a8bee
Shushanik Sargsyan <shushan.sargsyan@gmail.com>
2018-06-04 16:21:28 +0400

    normalizing query string, getContent function

--- core/Request.php
+++ core/Request.php
@@ -1,7 +1,8 @@
 <?php
-include 'Parameter.php';
-include 'Header.php';
+
+use Header;
 include 'functions.php';
+
 class Request
 {
     protected $parameters;
@@ -12,6 +13,7 @@ class Request
     protected $requestUri;
     protected $baseUrl;
     protected $method;
+    protected static $httpMethodParameterOverride = false;
 
     public function __construct(array $query = array(), array $request = array(), $content = null)
     {
@@ -27,7 +29,8 @@ class Request
         $this->method = null;
     }
 
-    public function  all() {
+    public function all()
+    {
         return $this->request->all();
     }
 
@@ -41,12 +44,22 @@ class Request
 
     public function hasHeader($key)
     {
-        return ! is_null($this->header($key));
+        return !is_null($this->header($key));
     }
 
     public function getMethod()
     {
-        $this->method = strtoupper($_SERVER['REQUEST_METHOD']);
+        if (null === $this->method) {
+            $this->method = $_SERVER['REQUEST_METHOD'] ? strtoupper($_SERVER['REQUEST_METHOD']) : 'GET';
+
+            if ('POST' === $this->method) {
+                if ($method = $this->headers->get('X-HTTP-METHOD-OVERRIDE')) {
+                    $this->method = strtoupper($method);
+                } elseif (self::$httpMethodParameterOverride) {
+                    $this->method = strtoupper($this->request->get('_method', 'POST'));
+                }
+            }
+        }
 
         return $this->method;
     }
@@ -59,5 +72,86 @@ class Request
     public function input($key = null, $default = null)
     {
         return data_get($this->request, $key, $default);
+    }
+
+    public static function normalizeQueryString($qs)
+    {
+        if ('' == $qs) {
+            return '';
+        }
+
+        $parts = array();
+        $order = array();
+
+        foreach (explode('&', $qs) as $param) {
+            if ('' === $param || '=' === $param[0]) {
+                continue;
+            }
+
+            $keyValuePair = explode('=', $param, 2);
+
+            $parts[] = isset($keyValuePair[1]) ?
+                rawurlencode(urldecode($keyValuePair[0])) . '=' . rawurlencode(urldecode($keyValuePair[1])) :
+                rawurlencode(urldecode($keyValuePair[0]));
+            $order[] = urldecode($keyValuePair[0]);
+        }
+
+        array_multisort($order, SORT_ASC, $parts);
+
+        return implode('&', $parts);
+    }
+
+    public static function enableHttpMethodParameterOverride()
+    {
+        self::$httpMethodParameterOverride = true;
+    }
+
+    public static function getHttpMethodParameterOverride()
+    {
+        return self::$httpMethodParameterOverride;
+    }
+
+    public function getQueryString()
+    {
+        $qs = static::normalizeQueryString($_SERVER['QUERY_STRING']);
+
+        return '' === $qs ? null : $qs;
+    }
+
+    public function isJson()
+    {
+        return contains($this->header('CONTENT_TYPE'), ['/json', '+json']);
+    }
+
+    public function contains($haystack, $needles)
+    {
+        foreach ((array)$needles as $needle) {
+            if ($needle != '' && mb_strpos($haystack, $needle) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function getContent()
+    {
+        if ($this->header(['CONTENT_TYPE']) == 'application/json') {
+            $this->content = json_decode(file_get_contents('php://input'));
+        } elseif ($this->header(['CONTENT_TYPE']) == 'application/x-www-form-urlencoded') {
+            switch ($this->getMethod()) {
+                case 'get':
+                    $this->content = $_GET;
+                    break;
+                case 'post':
+                    $this->content = $_POST;
+                    break;
+                case 'put':
+                case 'delete':
+                    parse_str(file_get_contents("php://input"), $this->content);
+                    $this->content = json_decode(json_encode($this->content));
+                    break;
+            }
+        }
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,22 +1,13 @@
 <?php
 
-//include "../core/Router.php";
-//include "../core/Request.php";
-
 function __autoload($classname) {
     $filename = "../core/". $classname .".php";
     include_once($filename);
 }
-
 $query = rtrim($_SERVER['QUERY_STRING'],'/');
-
 Router::add("^$", ['controller' => "Main", 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
 
-/** @var object $header */
-
-$header= new Header();
-$key = 'host';
-
-echo '<pre>',print_r($header->all()),'</pre>';
+mb_parse_str('email=kehaovista@qq.com&city=shanghai&job=Phper', $str);
+print_r($str);
 ?>
